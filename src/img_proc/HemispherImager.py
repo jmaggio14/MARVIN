9d58cfcf9ba90694a91100b88570cdff0d371e32
jmaggio14 <jmaggio14@gmail.com>
2017-12-01 20:50:13 -0800

    began working on a hemisphere imager

---
+++ src/img_proc/HemispherImager.py
@@ -0,0 +1,30 @@
+import marvin
+
+
+
+
+class HemisphereImager(object):
+    def __init__(self,cam_id1,cam_id2,cam_id3,cam_id4):
+        self.cap1 = marvin.CameraCapture( cam_id1 )
+        self.cap2 = marvin.CameraCapture( cam_id2 )
+        self.cap3 = marvin.CameraCapture( cam_id3 )
+        self.cap4 = marvin.CameraCapture( cam_id4 )
+
+        self.cache = {"frames":None,
+                      "panorama":None}
+
+    def read_frames(self):
+        """
+        reads frames off of each camera specified
+        input::
+            None
+        return::
+        """
+        frames = []
+        frames.append( self.cap1.read() )
+        frames.append( self.cap2.read() )
+        frames.append( self.cap3.read() )
+        frames.append( self.cap4.read() )
+        self.cache["frame"] = frames
+        return frames
+    def pana

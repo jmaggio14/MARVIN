3ef8acc9c9f9e9c15dfe1f24484aeeb745b76919
jmaggio14 <jmaggio14@gmail.com>
2017-12-02 17:08:42 -0800

    updates constants file

--- src/system/constants.py
+++ src/system/constants.py
@@ -2,10 +2,24 @@ import logging
 import os
 
 
-GPIO_OUTPUT = "out"
+"""
+CONSTANTS::
+    GPIO::
+        GPIO_INPUT = "in"
+        GPIO_OUTPUT = "out"
+        GPIO_ON = 1
+        GPIO_OFF = 0
+    LOGGER::
+        LOGGER_STATUS = 0
+        LOGGER_EVENT = 1
+"""
+#GPIO CONSTANTS
 GPIO_INPUT = "in"
-GPIO_HIGH = int(1)
-GPIO_LOW = int(0)
-#END
+GPIO_OUTPUT = "out"
+GPIO_HIGH = 1
+GPIO_LOW = 0
+
+
+#LOGGER CONSTANTS
 LOGGER_STATUS = 2
 LOGGER_EVENT = 3

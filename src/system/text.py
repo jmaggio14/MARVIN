65307bb16717b106e185ae675246f0aaeab31c32
jmaggio14 <jmaggio14@gmail.com>
2017-12-02 17:07:40 -0800

    more detailed docstring for the color text function

--- src/system/text.py
+++ src/system/text.py
@@ -1,6 +1,37 @@
 import termcolor
 
 def textColor(text,color="r",background=None,attrs=None):
+    """
+    adds ansi escape codes to a string to
+
+    input::
+        text (str): Input text to colorize
+        color (str): String which indicates the color to use for the text
+                acceptable values are:
+                                "r",    "red",
+                                "g",  "green",
+                                "y", "yellow",
+                                "b",   "blue",
+                                "m",magenta",
+                                "c",   "cyan",
+                                "w",  "white",
+        background (str): String which indicates what color to use for the text's
+                background colors. acceptable values are:
+                                "r",    "red",
+                                "g",  "green",
+                                "y", "yellow",
+                                "b",   "blue",
+                                "m",magenta",
+                                "c",   "cyan",
+                                "w",  "white",
+        attrs (list,str): additional styles that can be applied to the text. This can
+                        be in the form of a long string or list
+                acceptable values are:
+                                "bold","dark","underline","reverse","concealed"
+
+    return::
+        string with proper ansi codes attached
+    """
     color_codes = {
                 "r":"red",    "red":"red",
                 "g":"green",  "green":"green",

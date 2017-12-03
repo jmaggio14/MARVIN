bbb1787332826734fdc3c23ffbb27e0c9d59f112
jmaggio14 <jmaggio14@gmail.com>
2017-12-02 17:00:35 -0800

    trying to tackle sharing memory between python and C

--- src/C/pwm.c
+++ src/C/pwm.c
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 
 
-int pulseWidthModulation(int pin_number, uint32_t on_time,uint32_t off_time){
+int pulseWidthModulation(int pin_number, char* break_path_p, uint32_t on_time,uint32_t off_time){
     /*
     INCOMPLETE!
 
@@ -16,29 +16,26 @@ int pulseWidthModulation(int pin_number, uint32_t on_time,uint32_t off_time){
     */
     //creating file path
     char value_path[80];
-    char break_path[80];
     sprintf(value_path,"/sys/class/gpio/gpio%d/value",pin_number);
-    sprintf(break_path,"gpio%d_break_pipe",pin_number);
-    printf("value_path:%s\n",value_path );
-    printf("break_path:%s\n",break_path );
     char on_signal = '1';
     char off_signal = '0';
 
-    //creating file pointers that
-    FILE* fp;
-    fp = fopen(value_path,"w");
-    int fd = open(break_path,O_RDONLY | O_NONBLOCK);
-    ssize_t break_read_size;
-    int break_value = 0;
+    //creating file pointers for the gpio value file
+    FILE* value_fp = fopen(value_path,"w");
+    //creating file ids for the break_path_p pipe
+    int fd = open(*break_path_p, O_RDONLY | O_NONBLOCK);
+    char break_value = '0';
+    ssize_t break_value_size;
+
     while(1){
-      break_read_size = read(fd,&break_value,4);
-      if ( break_value == 1){
+      break_read_size = read(fd,&break_value,sizeof(char));
+      if (break_value == '1'){
         break;
       }
-      fwrite(&on_signal,1,1,fp);
+      fwrite(&on_signal,1,1,value_fp);
       usleep(on_time);
-      fwrite(&off_signal,1,1,fp);
-      usleep(on_time);
+      fwrite(&off_signal,1,1,value_fp);
+      usleep(off_time);
     }
     return 0;
 }

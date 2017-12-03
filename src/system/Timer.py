a5cd93ab803c773569e646637081588c38c45a8c
jmaggio14 <jmaggio14@gmail.com>
2017-12-02 17:04:16 -0800

    adds functionality to run function on an interval

--- src/system/Timer.py
+++ src/system/Timer.py
@@ -1,13 +1,17 @@
 import time
+import marvin
 
 class Timer(object):
     """
     Timer which can be used to time processes
 
     attributes::
-        _start: start time in seconds since the epoch
-        _last: last time the lap timer was called
-        _countdown: countdown time if set (recalculated with the countdown property)
+        _start (float): start time in seconds since the epoch
+        _last (float): last time the lap timer was called
+        _countdown (float): countdown time if set (recalculated with the countdown property)
+        _last_countdown (float): last countdown time check
+        __interval_countdown (float): current interval_countdown time
+        __last_interval_countdown (float): last interval countdown time
 
     functions::
         reset(): resets the timer start time
@@ -17,20 +21,71 @@ class Timer(object):
         lap: time since lap was last accessed (or when timer was created for the first access)
         countdown: countdown time, recalculated every time it's called. never below 0
         countdown.setter: sets the value of countdown
+        __interval_countdown: private interval countdown timer for the interval timing
+        __interval_countdown.setter: sets the value of interval_countdown
 
     """
     def __init__(self):
         self._start = time.time()
         self._last = self._start
+
         self._last_countdown = float(0)
         self._countdown = float(0)
+
+        self._last_interval_countdown = float(0)
+        self._interval_countdown = float(0)
 
     def reset(self):
         """ resets the timer start time """
         self.__init__()
 
-    def set_countdown(self,value):
-        self.countdown = value
+    def runOnInterval(self,interval,func,args,kwargs,Exit_Status=None):
+        """
+        CURRENTLY HAS AN ISSUE WHERE TIMING TAKES ~50%% longer than the interval specifies!
+
+        runs a fiven function on a specified interval. The user can exit this
+        loop using a marvin.ExitStatus object
+
+        while this is operating, the countdown clock is not operational. As with most
+        things in python, the timing should be not be relied on for
+
+        input::
+            interval (float): time in seconds between function calls
+            func (func pointer): pointer to the function to be called on every interval
+            args (packed list): list of arguments for 'func'
+            kwargs (packed dict): dictionary of keyword arguments for 'func'
+            Exit_Status (marvin.ExitStatus): marvin.ExitStatus object,
+                if None:: then a placeholder object will be created and the loop will run forever
+                if int:: function will be run that number of iterations
+        return::
+            None
+        """
+        def run():
+            # print(1)
+            # print(self.__interval_countdown)
+            while self.__interval_countdown:
+                continue
+            else:
+                func(*args,**kwargs)
+                self.__interval_countdown = interval
+
+        #checking
+        if Exit_Status == None:
+            while True:
+                run()
+
+        #checking for number of iterations
+        elif isinstance(Exit_Status,(int,float)):
+            Exit_Status = int(Exit_Status)
+            for i in range(Exit_Status):
+                run()
+
+        #checking if it's an ExitStatus Queue
+        elif isinstance(Exit_Status,marvin.ExitMonitor):
+            while not Exit_Status.exit_status:
+                run()
+        else:
+            marvin.Status.critical("run on interval has an unknown exit strategy, (use None for infinite)!!")
 
     @property
     def time(self):
@@ -58,6 +113,20 @@ class Timer(object):
         """sets the countdown timer"""
         if isinstance(value,(int,float)):
             self._countdown = float(value)
+
+    @property
+    def __interval_countdown(self):
+        """returns the current interval_countdown time"""
+        self._interval_countdown = float(self._interval_countdown) - (self.time - self._last_interval_countdown)
+        self._last_interval_countdown = self.time
+        if self._interval_countdown < 0:
+            self._interval_countdown = 0
+        return self._interval_countdown
+    @__interval_countdown.setter
+    def __interval_countdown(self,value):
+        """sets the interval_countdown timer"""
+        if isinstance(value,(int,float)):
+            self._interval_countdown = float(value)
 
     @property
     def start(self):

398cf0d96ff4c88bb7be1e7f7e466c81334def53
jmaggio14 <jmaggio14@gmail.com>
2017-12-27 16:15:19 -0800

    adds new numpy array with frame_ids

--- src/img_proc/Image.py
+++ src/img_proc/Image.py
@@ -1,32 +1,31 @@
 import numpy as np
-# import marvin
 import copy
 
 class MarvinImage(np.ndarray):
-    def __new__(cls,input_array,frame_id):
+    """
+    An image class which inherits from numpy arrays
+    
+    """
+    def __new__(cls,input_array,frame_id,ORB=None):
         # building a numpy array and augmenting
         obj = np.asarray(input_array).view(cls)
-        obj.src = copy.copy(obj)
-
-        obj.id = str(frame_id)
+        obj.id = frame_id
         return obj
 
     def __array_finalize__(self,obj):
-        if obj is None: return
-        # print(dir(obj))
-        self.src = getattr(obj,"src",None)
+        if obj is None: return None
         self.id = getattr(obj,"id",None)
 
     def __array_wrap__(self,out_arr,context=None):
-        return super(MarvinImage,self).__array_wrap__(self,out_arr,context)
-
+        return np.ndarray.__array_wrap__(self,out_arr,context)
+    # #
+    #
 
 if __name__ == "__main__":
     test_src = np.random.rand(512,512)
     test_Image = MarvinImage(test_src,10)
 
     print("test_Image:\n",test_Image)
-    print("test_Image.src:\n",test_Image.src)
     print("test_Image.src == test_Image", test_Image.src == test_Image)
 
     modified_test_image = test_Image * 10

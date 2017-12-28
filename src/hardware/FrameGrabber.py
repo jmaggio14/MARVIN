bedc209a1801a1ad36fe4b5b2a938c72230c209a
jmaggio14 <jmaggio14@gmail.com>
2017-12-27 16:14:14 -0800

    adds FrameGrabber object

--- src/hardware/FrameGrabber.py
+++ src/hardware/FrameGrabber.py
@@ -10,17 +10,38 @@ class FrameGrabber(object):
                 for more information
         cache_size (int): the number of frames to cache for each camera in the buffer
         fourccs (list): a list of strings to override the default MJPG fourcc code
-    """
-    def __init__(self,cam_ids,cache_size=30,fourccs=None):
-        self.cam_ids = map(str,cam_ids)
 
-        self.cache_size = int(cache_size)
+    attributes::
+        cam_ids (list): list of input camera ids, turned to strings
+        buffer_size (int): input buffer_size, cast to an integer
+        fourccs (list): list of strings, copied from input or if None, then
+                    equal to ["MJPG"] * len(self.cam_ids)
+        caps (list): list of marvin.CameraCapture objects for each camera, direct output
+                    from __createCaptureObjects()
+        buffers (list): list of marvin.CaptureBuffer objects, where the output from
+                    each cameras is saved
+
+
+    functions::
+        __createCaptureObjects(): creates the marvin.CameraCapture objects used to
+                    capture images from the camera
+        __createBuffers(): creates the marvin.CaptureBuffer objects used to store
+                    images off the cameras
+    """
+    def __init__(self,cam_ids,buffer_size=30,fourccs=None):
+        # self.cam_ids = map(str,cam_ids)
+        self.cam_ids = []
+        for i in cam_ids:
+            self.cam_ids.append( str(i) )
+
+        self.buffer_size = int(buffer_size)
 
         if fourccs == None:
             fourccs = ["MJPG"] * len(self.cam_ids)
         self.fourccs = fourccs
 
         self.caps = self.__createCaptureObjects()
+        self.buffers = self.__createBuffers()
 
     def __createCaptureObjects(self):
         """
@@ -38,35 +59,44 @@ class FrameGrabber(object):
         for cam_id,fourcc in self.cam_ids,self.fourccs:
             try:
                 caps[cam_id].append( marvin.CameraCapture(cam_id,fourcc) )
+                marvin.Status.info("adding CameraCapture object for cam_id: {0}".format(cam_id))
+
             except marvin.MarvinCameraException:
                 marvin.Status.critcal("unable to connect to camera: {0} with fourcc: {1}".format(cam_id,fourcc))
 
-    def __setupBuffers(self):
+        return caps
+
+    def __createBuffers(self):
         """
         private to marvin.FrameGrabber
 
-        creates the buffers
+        creates the buffers necessary for temporary storage of frames
+        input::
+            None
+        return::
+            buffers (dict): dict of marvin.CaptureBuffer objects created for each
+                            camera to store the frames in a temporary manner
+                            the key is the corresponding frame id
         """
+        buffers = {}
+        for cam_id in self.caps.keys():
+            buffers[cam_id] = marvin.CaptureBuffer(self.buffer_size)
+            marvin.Status.info("adding CaptureBuffer object for cam_id: {0}".format(cam_id))
+        return buffers
 
 
 
+    def grab(self):
+        """
+        grabs images from all the cameras, puts them in Frame Containers, and
+        stores them in the buffers
 
 
-class CaptureBuffer(object):
-    def __init__(self,cache_size):
-        self.cache_size = cache_size
-
-    def add(self,):
-
-
-
-
-
-
-
-
-
-
+        """
+        for cam_id in self.caps.keys():
+            raw_frame,raw_frame_metadata = self.caps[cam_id].readFrameAndMetadata()
+            frame_container = marvin.FrameContainer(raw_frame,raw_frame_metadata)
+            self.buffers[cam_id].add( frame_container )
 
 
 

3545c32ca2540c537b981546ac26bb47ade32c29
jmaggio14 <jmaggio14@gmail.com>
2018-01-21 13:09:03 -0800

    adds unit tests for Capture Buffers

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -65,6 +65,79 @@ def test_quickImageView():
 
 
 
+def test_CaptureBuffer():
+    marvin.init()
+    testing_logger = marvin.getLogger("TESTING LOGGER",log_level=10,logger_type=marvin.LOGGER_EVENT)
+
+    # generateRandomImage = lambda : np.random.rand(512.512)
+
+    testing_logger.info("creating camera capture object")
+    cap = marvin.CameraCapture()
+
+    testing_logger.info("creating the image viewers")
+    cap_viewer = marvin.ImageViewer("raw off the cameras")
+    buffer_viewer = marvin.ImageViewer("capture buffer test")
+
+    testing_logger.info("creating the capture buffer")
+    buf = marvin.CaptureBuffer()
+
+    timer = marvin.Timer()
+    timer.countdown = 30
+    testing_logger.info("setting countdown timer to 30seconds")
+
+    while timer.countdown:
+        frame = cap.read()
+        cap_viewer.view(frame)
+        #adding the frame to the buffer
+        frame_id = buf.add(frame)
+        buffer_viewer.view( buf.getByIndex(15) )
+        print("countdown: ",timer.countdown)
+    buffer_viewer.close()
+    cap_viewer.close()
+
+
+def test_debugFrame():
+    marvin.init()
+    testing_logger = marvin.getLogger("TESTING LOGGER",log_level=10,logger_type=marvin.LOGGER_EVENT)
+
+    debug_frame = marvin.debugFrame("test debugging frame")
+    marvin.quickImageView( debug_frame )
+
+
+
+
+def test_MultiCam():
+    marvin.init()
+    testing_logger = marvin.getLogger("TESTING LOGGER",log_level=10,logger_type=marvin.LOGGER_EVENT)
+
+    frame_grabber = marvin.MultiCam( ["/dev/video0"],buffer_size=30,fourccs=["MJPG"] )
+    cam_id = frame_grabber.cam_ids[0]
+    real_time_viewer = marvin.ImageViewer("MultiCam test -- real time viewer")
+    index_15_viewer = marvin.ImageViewer("MultiCam test -- index 15 viewer")
+
+    for i in range(500):
+        frame_set = frame_grabber.grab()
+        testing_logger.info("grabbing frame {0}, total number of frames in one camera buffer {1}".format(i,len(frame_grabber.frame_sets)) )
+        real_time_viewer.view( frame_grabber.retrieve( frame_set )[cam_id] )
+        index_15_viewer.view( frame_grabber.retrieve(15)[cam_id] )
+
+    index_15_viewer.close()
+    real_time_viewer.close()
+
+
+def test_Rtsp():
+    marvin.init()
+    testing_logger = marvin.getLogger("TESTING LOGGER",log_level=10,logger_type=marvin.LOGGER_EVENT)
+
+    incrementor = marvin.DebugFrameIncrementor()
+    streamer = marvin.RtspServer(host="127.0.0.1",port=5000)
+    viewer = marvin.ImageViewer("RTSP SERVER TEST -- these images will be sent to the server")
+
+    for i in range(10000):
+        frame = incrementor.next()
+        streamer.write(frame)
+        viewer.view(frame)
+
 if __name__ == "__main__":
     marvin.init()
 

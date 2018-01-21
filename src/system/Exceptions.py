4e15fd3a46bd6007efbc9199b35f9731d6dbdfa2
jmaggio14 <jmaggio14@gmail.com>
2018-01-21 13:06:50 -0800

    adds a new FrameTypeError Exception

--- src/system/Exceptions.py
+++ src/system/Exceptions.py
@@ -38,3 +38,15 @@ class MarvinCameraException(Exception):
         cam_string = str(cam_id) if not marvin.typeCheck(cam_id,None) else "UNKNOWN"
         error_message = """problem reading frame off of camera '{0}'. is camera connected""".format(cam_string)
         print(marvin.textColor(error_message,'r',None,'bold'))
+#
+# class FrameTypeError(Exception):
+#     """
+#     Exception meant to be raised when an incorrect type is passed into
+#
+#     does NOT raise a SystemExit
+#     """
+#     def __init__(self,cam_id=None,*args,**kwargs):
+#         super(MarvinCameraException,self).__init__(*args,**kwargs)
+#         cam_string = str(cam_id) if not marvin.typeCheck(cam_id,None) else "UNKNOWN"
+#         error_message = """problem reading frame off of camera '{0}'. is camera connected""".format(cam_string)
+#         print(marvin.textColor(error_message,'r',None,'bold'))
